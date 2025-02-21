75a5f098f8e7cbfce1f061fd1052e62e31318e10
Gandavi <kirill.budchenko.03@mail.ru>
2025-02-21 18:01:03 +0300

    first

---
+++ data.js
@@ -0,0 +1,328 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7547662958082935,
+          "pitch": 0.007458180863279651,
+          "rotation": 0,
+          "target": "3-3"
+        },
+        {
+          "yaw": 2.321057890899331,
+          "pitch": 0.021955652220949418,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3511169941070431,
+          "pitch": 0.07886483771307695,
+          "title": "ёлка",
+          "text": "<span style=\"color: rgba(255, 255, 255, 0.96); font-family: &quot;SB Sans Text&quot;, sans-serif; font-size: 16px; letter-spacing: -0.32px; background-color: rgba(26, 29, 34, 0.9);\">вечнозелёное хвойное дерево семейства сосновых (Pinaceae). Существует несколько видов елей, наиболее распространённые из которых — ель обыкновенная (Picea abies), ель сибирская (Picea obovata) и ель колючая (Picea pungens).</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9025600092930404,
+          "pitch": -0.008200195162450186,
+          "rotation": 0,
+          "target": "3-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.039390817759635155,
+          "pitch": 0.045773048767379976,
+          "rotation": 0,
+          "target": "7-15"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.41841647148133987,
+          "pitch": 0.028855879572269316,
+          "rotation": 0,
+          "target": "2-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4036896072710814,
+          "pitch": 0.022481721948636846,
+          "title": "зелёный лог",
+          "text": "<br><div><span style=\"color: rgba(255, 255, 255, 0.96); font-family: &quot;SB Sans Text&quot;, sans-serif; font-size: 16px; letter-spacing: -0.32px; background-color: rgba(26, 29, 34, 0.9);\">Парк в микрорайоне Зелёный Лог в Старом Осколе является одним из любимых мест отдыха местных жителей.&nbsp;</span><span style=\"background-color: rgba(26, 29, 34, 0.9); color: rgba(255, 255, 255, 0.96); font-family: &quot;SB Sans Text&quot;, sans-serif; font-size: 16px; letter-spacing: -0.32px;\">Этот парк был создан с целью обеспечения комфортного пространства для прогулок, занятий спортом и проведения досуга.</span></div>"
+        }
+      ]
+    },
+    {
+      "id": "4-40",
+      "name": "40",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4887588481443723,
+          "pitch": 0.011952922186690529,
+          "rotation": 0,
+          "target": "6-20"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-19",
+      "name": "19",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19427806947022574,
+          "pitch": -0.0024422020065255623,
+          "rotation": 0,
+          "target": "4-40"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-20",
+      "name": "20",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-15",
+      "name": "15",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03086884832710446,
+          "pitch": -0.0032599657373104662,
+          "rotation": 0,
+          "target": "5-19"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
